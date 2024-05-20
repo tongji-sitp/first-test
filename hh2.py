174d8d10e4e32d7c7a6bf7c923fc600d7772e84d
HHxxr <2353967@tongji.edu.cn>
2024-05-20 23:51:43 +0800

    Update hh2.py

--- hh2.py
+++ hh2.py
@@ -1,33 +1,68 @@
->>> import pybliometrics
->>> pybliometrics.init()  # read API keys
->>> # Document-specific information
->>> from pybliometrics.scopus import AbstractRetrieval
->>> ab = AbstractRetrieval("10.1016/j.softx.2019.100263")
->>> ab.title
-'pybliometrics: Scriptable bibliometrics using a Python interface to Scopus'
->>> ab.publicationName
-'SoftwareX'
->>> ab.authors
-[Author(auid=57209617104, indexed_name='Rose M.E.', surname='Rose',
- given_name='Michael E.', affiliation='60105007'),
- Author(auid=7004212771, indexed_name='Kitchin J.R.', surname='Kitchin',
- given_name='John R.', affiliation='60027950')]
->>> 
->>> # Author-specific information
->>> from pybliometrics.scopus import AuthorRetrieval
->>> au2 = AuthorRetrieval(ab.authors[1].auid)
->>> au2.h_index
-34
->>> au1 = AuthorRetrieval(ab.authors[0].auid)
->>> au1.affiliation_current
-[Affiliation(id=60105007, parent=None, type='parent', relationship='author',
- afdispname=None, preferred_name='Max Planck Institute for Innovation and Competition',
- parent_preferred_name=None, country_code='deu', country='Germany',
- address_part='Marstallplatz 1', city='Munich', state='Bayern',
- postal_code='80539', org_domain='ip.mpg.de', org_URL='http://www.ip.mpg.de/')]
->>> 
->>> # Affiliation information
->>> from pybliometrics.scopus import AffiliationRetrieval
->>> aff1 = AffiliationRetrieval(au1.affiliation_current[0].id)
->>> aff1.author_count
-98
+class Literature:
+    def __init__(self, doi):
+        self.doi = doi
+        self.title = None
+        self.authors = None
+        self.abstract = None
+        self.keywords = None
+        self.citation_count = None
+
+    def query(self, scopus_api):
+        # 使用ScopusAPI查询文献信息并更新属性
+        info = scopus_api.get_literature_info(self.doi)
+        if info:
+            self.title = info['title']
+            self.authors = info['authors']
+            self.abstract = info['abstract']
+            self.keywords = info['keywords']
+            self.citation_count = info['citation_count']
+
+    def update(self, scopus_api):
+        # 使用ScopusAPI更新文献信息并更新属性
+        # 注意：Scopus API可能不支持直接更新文献信息
+        pass
+
+    def store(self, db):
+        # 将文献信息存储到数据库
+        db.store_literature(self)
+
+
+class Database:
+    def __init__(self, db_path):
+        self.db_path = db_path
+        self.db = TinyDB(db_path)
+
+    def store_literature(self, literature):
+        # 存储文献信息到数据库
+        self.db.insert(literature.__dict__)
+
+    def update_literature(self, literature):
+        # 更新数据库中的文献信息
+        # 注意：需要根据TinyDB的文档来正确实现更新逻辑
+        pass
+
+    def query_literature(self, doi):
+        # 根据DOI从数据库中查询文献信息
+        # 注意：需要根据TinyDB的文档来正确实现查询逻辑
+        pass
+
+
+class ScopusAPI:
+    def __init__(self, api_key):
+        self.api_key = api_key
+
+    def get_literature_info(self, doi):
+        # 发送GET请求到Scopus API，获取文献信息
+        url = f"https://api.elsevier.com/content/abstract/doi/{doi}"
+        headers = {'Accept': 'application/json', 'X-ELS-APIKey': self.api_key}
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            print(f"Error: {response.status_code}")
+            return None
+
+    def update_literature_info(self, doi):
+        # 如果API支持，发送更新请求到Scopus API
+        # 注意：Scopus API可能不支持直接更新文献信息
+        pass
